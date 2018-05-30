c063971048b7711793daa90f0293c4c1bac014e5
Seb <sebastien.le-gall@epitech.eu>
2018-05-30 18:25:36 +0200

    cleanner systems file and components file

---
+++ srcs/Graphical/ComponentCamera.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2021
+** Client
+** File description:
+** Created by seb,
+*/
+
+#pragma once
+
+#include <irrlicht/irrlicht.h>
+#include <ECS/Entity/Entity.hpp>
+#include <ECS/Component/Component.hpp>
+#include "ComponentBeing.hpp"
+
+namespace ecs::component {
+	struct Camera {
+		Camera(irr::scene::ISceneManager *smgr, ecs::entity::Id id)
+		{
+			irr::core::vector3df pos = ecs::component::Manager<ecs::component::Being>::get()[id]._node->getPosition();
+			_camera = smgr->addCameraSceneNode();
+			_camera->setPosition(irr::core::vector3df(pos.X + 0, pos.Y + 500,pos.Z - 500));
+			_camera->setTarget(pos);
+		};
+
+		irr::scene::ICameraSceneNode* _camera;
+	};
+}
