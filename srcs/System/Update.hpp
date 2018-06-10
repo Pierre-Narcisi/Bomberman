2da1f404f69451c221e4b51555c849af85890bf3
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-06-10 17:18:07 +0200

    windows compatible

--- srcs/System/Update.hpp
+++ srcs/System/Update.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <math.h>
+
 #include "Settings/Inputs.hpp"
 #include "ECS/Entity/Filter.hpp"
 
@@ -31,8 +33,8 @@ namespace ecs { namespace system {
 
 			for (auto &id : fl.list) {
 				if (key[id].attack.state) {
-					pos.Y = being[id]._node->getPosition().Z + 50 - std::fmod(static_cast<double>(being[id]._node->getPosition().Z + 50) , 100.0);
-					pos.X = being[id]._node->getPosition().X + 50 - std::fmod(static_cast<double>(being[id]._node->getPosition().X + 50) , 100.0);
+					pos.Y = being[id]._node->getPosition().Z + 50 - ::fmod(static_cast<double>(being[id]._node->getPosition().Z + 50) , 100.0);
+					pos.X = being[id]._node->getPosition().X + 50 - ::fmod(static_cast<double>(being[id]._node->getPosition().X + 50) , 100.0);
 					ecs::system::Create::createBomb(id, pos);
 				}
 			}
@@ -47,6 +49,8 @@ namespace ecs { namespace system {
 
 			_switch = !_switch;
 			if (_switch == true) {
+			
+			
 				ecs::system::Scene::loadMenuInGame();
 				/* create menu */
 			} else {
