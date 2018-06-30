bc554fda541da87749c053adda3a2e2d98a7c721
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-06-30 14:27:05 +0200

    Fix game launcher + add .deb

--- Launcher/gamelauncher.cpp
+++ Launcher/gamelauncher.cpp
@@ -8,6 +8,7 @@ void    GameLauncher::start(QString const &path, QString const &binaryName)
     QFile               programFile(path + "/" + binaryName);
 
     programFile.setPermissions(programFile.permissions() | QFile::ExeUser);
+    bool bSet = qputenv("LD_LIBRARY_PATH", ".");
     if (QProcess::startDetached(programFile.fileName(), {nullptr}, path) == false)
        std::cerr << "failed to start " << (path + "/" + binaryName).toStdString() << std::endl;
 }
